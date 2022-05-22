cb0c5ce30c25eef4a8ae62f4b7a5abc31ae3a465
suyash-srivastava-dev <Git@New@1>
2022-05-22 19:40:35 +0530

    updated

--- src/main/java/Courses/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package Courses.repo;
-
-import Courses.pojo.Employee;
-import org.springframework.data.repository.CrudRepository;
-//all the CRUD operations are already there by CrudRepository
-
-public interface EmployeeRepo extends CrudRepository<Employee,String> {
-
-}
