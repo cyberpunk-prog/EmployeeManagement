6e6cf681082e1dd6ae272f44007df54d3968f8c7
suyash-srivastava-dev <Git@New@1>
2022-05-22 20:52:48 +0530

    updated db

--- src/main/java/com/example/demoproject1/repo/EmployeeRepo.java
+++ src/main/java/com/example/demoproject1/repo/EmployeeRepo.java
@@ -1,9 +1,10 @@
 package com.example.demoproject1.repo;
 
 import com.example.demoproject1.pojo.Employee;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 //all the CRUD operations are already there by CrudRepository
 
-public interface  EmployeeRepo extends CrudRepository<Employee,String> {
+public interface  EmployeeRepo extends JpaRepository<Employee,String> {
 
 }
